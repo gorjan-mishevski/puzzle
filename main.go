a303855c5f205a89f52531359d21316753becd34
Gorjan Mishevski <gogozvermk@gmail.com>
2017-12-20 13:46:11 +0100

    Init

---
+++ main.go
@@ -0,0 +1,66 @@
+package puzzle
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+// Get the price.
+// {
+// 	"ETH": {
+// 		"USD": 736.83,
+// 		"EUR": 619.8
+// 	},
+// 	"BTC": {
+// 		"USD": 18565.05,
+// 		"EUR": 15757.92
+// 	},
+// 	"REP": {
+// 		"USD": 47.1,
+// 		"EUR": 39.39
+// 	}
+// }
+// https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,BTC,REP&tsyms=USD,EUR
+
+var (
+	baseHref = "https://min-api.cryptocompare.com/data"
+)
+
+// MultiPrice - Calculates the value for the searched currency. Return example: unpacked["ETH"]["USD"] gives the value ETH -> USD.
+func MultiPrice(from []string, to []string) (unpacked map[string]map[string]float64) {
+	url := fmt.Sprintf("%s/pricemulti?fsyms=%s&tsyms=%s", baseHref, strings.Join(from, ","), strings.Join(to, ","))
+
+	client := http.Client{
+		Timeout: time.Second * 5, // 5 seconds
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal("Can't create new request for multi price. \n", err)
+	}
+
+	req.Header.Add("User-Agent", "web-app")
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Failed feching from API. \n", err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("Failed reading body. \n", err)
+	}
+
+	err = json.Unmarshal(body, &unpacked)
+	if err != nil {
+		log.Fatal("Failed unmarshaling data. \n", err)
+	}
+
+	return
+}
